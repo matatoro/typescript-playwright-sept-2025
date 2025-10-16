599677f416e2a1c63bd60172235e0399e1e1af23
Nikolay Vasilev <the.matatoro@gmail.com>
2025-10-16 19:55:19 +0300

    Arrays in TS

--- arrays.ts
+++ arrays.ts
@@ -24,3 +24,32 @@ let checkTypeof = arrMixed.map((element: number | string) => {
 })
 
 console.log(checkTypeof)
+
+//16.10
+//
+// filter
+const numbers: number[] = [1, 5, 8, 3, 10, 2];
+
+const filteredNumbers: number[] = numbers.filter((num: number) => {
+  return num > 5;
+});
+
+console.log(filteredNumbers);
+// sort
+const sortNums: number[] = [100, 79, 121, 112, 3];
+
+console.log(sortNums.sort());
+sortNums.sort((a, b) => a - b);
+console.log(sortNums);
+//push
+sortNums.push(200)
+console.log(sortNums);
+//pop
+sortNums.pop()
+console.log(sortNums);
+//shift
+sortNums.shift()
+console.log(sortNums);
+//unshift
+sortNums.unshift(1)
+console.log(sortNums);
