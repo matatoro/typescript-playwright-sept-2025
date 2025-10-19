d8a56a980e5a2ce54d2b691de7096ddfc9924692
Nikolay Vasilev <the.matatoro@gmail.com>
2025-10-19 21:05:35 +0300

    TS Intro homework

---
+++ homeworks/ts-intro-homework.ts
@@ -0,0 +1,18 @@
+// Task 1
+const name: string = 'Peter';
+const age: number = 20;
+console.log(`Hello! I am ${name}, ${age} years old!`)
+
+//Task 2
+const numOne: number = 3;
+const numTwo: number = 22;
+const addition: number = numOne + numTwo;
+const substraction: number = numOne - numTwo;
+
+console.log(`${numOne} + ${numTwo} == ${addition}`)
+console.log(`${numOne} - ${numTwo} == ${substraction}`)
+
+//Task 3
+const num: number = 12;
+const isWithinRange: boolean = num >= 10 || num <= 50;
+console.log(isWithinRange)
